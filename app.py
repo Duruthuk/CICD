f74d37eb75693b3b4495251954cdbf020abab70f
Duruthu <duruthu2@gmail.com>
2026-09-20 11:48:09 +0530

    Fix Bandit security issue

--- app.py
+++ app.py
@@ -19,6 +19,6 @@ def create_profile_message(username, role="student"):
 if __name__ == "__main__":
  print(create_profile_message("student_01"))
 
-def show_directory_contents():
- """Intentionally insecure example for security testing."""
- subprocess.call("dir", shell=True)
+ def directory_contents():
+    """Safer version for Windows."""
+ subprocess.run(["cmd", "/c", "dir"], check=True)
